5e9319f979085e6f627782ce5e4a8fa9dbd53240
kiselpd <91218284+kiselpd@users.noreply.github.com>
2022-04-02 08:10:16 +0300

    version 0.3.1

--- main.c
+++ main.c
@@ -3,11 +3,9 @@
 
 #include "utils.h"
 #include "search.h"
-#include "show.h"
 
 int main()
 {
-    int mode;
     int result = EXIT;
     file_t files[500];
     dir_t curr_dir;
@@ -19,21 +17,26 @@ int main()
         show_error(result);
         return 0;
     }
+    if (result == DIR_NOT_FOUND)
+    {
+        free(curr_dir.dir_name);
+        show_error(result);
+        return 0;
+    }
         
     find_str = get_find_str(find_str);
     if (find_str == NULL)
     {
         result = INPUT_ERROR;
+        free(curr_dir.dir_name);
+        closedir(curr_dir.dir);
         show_error(result);
         return 0;
     }
 
-    printf("Input mode 0/1:");         //0-
-    scanf("%d", &mode);
+    result = search_top(curr_dir, find_str, files);
 
-    result = search_top(curr_dir, find_str, files, mode);
-
-    if ((result == INPUT_ERROR) || (result == DIR_NOT_FOUND) || (result == STR_NOT_FOUND))
+    if ((result == INPUT_ERROR) || (result == DIR_NOT_FOUND) || (result == STR_NOT_FOUND) || (result == MODE_ERROR) || (result == PID_ERROR) || (result == MALLOC_ERROR) || (result == MAP_ERROR))
     {
         show_error(result);
     }
---
+++ search_lib/regular_lib/include/utils.h
@@ -0,0 +1,49 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <dirent.h>
+
+typedef enum error_t
+{
+    MODE_ERROR = -7,
+    PID_ERROR = -6,
+    MALLOC_ERROR = -5,
+    MAP_ERROR = -4,
+    STR_NOT_FOUND = -3,
+    DIR_NOT_FOUND = -2,
+    INPUT_ERROR = -1
+}error_t;
+
+typedef enum value_t
+{
+    EXIT = 0,
+    TOP5 = 5
+}value_t;
+
+typedef enum search_mode_t
+{
+    SEQUENTIAL = 0,
+    PARALLEL = 1
+}search_mode_t;
+
+typedef struct directory_t
+{
+    char* dir_name;
+    DIR* dir;
+}dir_t;
+
+typedef struct file_t
+{
+    char* file_name;
+    size_t count_search;
+}file_t;
+
+int get_dir_t(dir_t* ptr_dir);
+
+void show_error(int error);
+
+char* get_find_str(char* find_str);
+
+void show_top(file_t files[], int count);
+
+#endif
